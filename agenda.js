68f1160ae602838a3af972b8dea82bf5ec330ed3
MURILO MACHADO BRANDALEZI <25170098@educ123.sp.senai.br>
2025-05-09 10:02:32 -0300

    add of files

--- agenda.js
+++ agenda.js
@@ -1,61 +1,66 @@
 const readline = require("readline-sync")
 const fs = require("fs");
-const cadastros = {};
-const agenda= {};
+
+
+
+const verde = "\x1b[32m";
+const vermelho = "\x1b[31m";
+const amarelo = "\x1b[33m";
+const azul = "\x1b[34m";
+const reset = "\x1b[0m";
+
+let cadastros = {};
+
 function menu() {
     console.clear()
     console.log("**Agenda**\n")
 
-    const opcao = Number(readline.question("\ 1. cadastrar usuarios \n 2. mostrar dados de um usuario\n 3. listar todos os nomes cadastrados \n 4. remover usuario \n 5. limpar toda agenda  \n 6. Gravar arquivo \n 7. ler arquivo \n 8. sair\n opcao: "))
+    const opcao = Number(readline.question("\ 1. cadastrar usuarios \n 2. mostrar dados de um usuario\n 3. listar todos os nomes cadastrados \n 4. remover usuario \n 5. limpar toda agenda  \n 6. Gravar arquivo \n 7. ler arquivo \n 8. sair\n\n\n opcao: "))
     return opcao 
 
     
-}while(true){
-    opcao = menu()
-    if (opcao == 1) {
-        adicionar()
-    }
-    else if (opcao == 2) {
-        listarUser()
-    }
-    else if (opcao == 3) {
-        listar()
-
-    }
-    else if(opcao == 4){
-        remover()
-    }
-    else if(opcao == 5){
-        limpar()
-    }
-    else if(opcao == 6){
-        gravar()
-    }
-    else if(opcao == 8){
-      break;
-    }
-    else{
-        console.log("Opção inválida!");
-}
 }
 
 function adicionar() {
-    console.clear()
-    console.log("**Cadastrar Usuario**\n")
-    const nome = readline.question("Digite o nome: ")
-    const telefone = readline.question("Digite o Telefone: ")
-    const email = readline.question("Digite o Email: ")
-    cadastros[nome] = { telefone, email, };
+    console.clear();
+    console.log(verde, "**Cadastrar Usuario**\n", reset);
+
+    let nome;
+    let telefone;
+    let email;
+
+   
+    do {
+        nome = readline.question("Digite o nome: ")
+        if (nome === "") {
+            console.log("O nome não pode estar em branco. Tente novamente.");
+        }
+    } while (nome === "");
+
+   
+    do {
+        telefone = readline.question("Digite o Telefone: ")
+        if (telefone === "") {
+            console.log("O telefone não pode estar em branco. Tente novamente.");
+        }
+    } while (telefone === "");
+
+   
+    do {
+        email = readline.question("Digite o Email: ")
+        if (email === "") {
+            console.log("O email não pode estar em branco. Tente novamente.");
+        }
+    } while (email === "");
+
+   
+    cadastros[nome] = { telefone, email };
     console.log("Usuário cadastrado com sucesso!");
-    
-
-
-
 }
 
 function listarUser() {
     console.clear()
-    console.log("**Mostar dados de um usuario**\n")
+    console.log(amarelo, "**Mostar dados de um usuario**\n", reset)
 
     const nome = readline.question("Informe o nome do usuario: ")
     const user = cadastros[nome];
@@ -78,17 +83,21 @@ readline.question("\nEnter para continuar!");
 
 
 function listar() {
-    console.clear()
-    console.log("**Listar todos os cadastros**\n")
-       for (const nome in cadastros) {
-            console.log(`* ${nome}`);
-            if(nome.lenght===0){
-                console.log("ola")
-            }
-        }
-    
-  
-readline.question("\nEnter para continuar!");
+    console.clear();
+    console.log(verde, "**Listar todos os cadastros**\n", reset);
+
+    let temCadastro = false;
+
+    for (const nome in cadastros) {
+        temCadastro = true;
+        console.log(`* ${nome}`);
+    }
+
+    if (temCadastro== false) {
+        console.log("Nenhum cadastro encontrado.");
+    }
+
+    readline.question("\nEnter para continuar!");
 }
 
 
@@ -96,7 +105,7 @@ readline.question("\nEnter para continuar!");
 
 function remover() {
     console.clear()
-    console.log("**Remover um usuario**\n")
+    console.log(vermelho, "**Remover um usuario**\n", reset)
     const del=readline.question("Qual usuario voce deseja remover? ")
     const user = cadastros[del];
     if (user) {
@@ -116,10 +125,17 @@ readline.question("\nEnter para continuar!");
 
 function limpar() {
     console.clear()
-    console.log("**Limpar a agenda**\n")
-     for(nome in cadastros){
-        delete cadastros[nome]
-     }
+    console.log(amarelo, "**Limpar a agenda**\n", reset)
+   const resposta= readline.question("Voce tem certeza que deseja limpar toda a agenda(S/N)? ")
+   if (resposta === "S" || resposta === "s" || resposta === "sim") {
+    for (let nome in cadastros) {
+        delete cadastros[nome];
+    }
+    console.log("Agenda limpa com sucesso!");
+} else {
+    console.log("Operação cancelada.");
+}
+
       
     
 readline.question("\nEnter para continuar!");
@@ -130,17 +146,67 @@ readline.question("\nEnter para continuar!");
 
 function gravar() {
     console.clear()
-    console.log("**Gravar a agenda**\n")
-    const file="agenda.json"
-    const conteudoGravar = JSON.stringify(agenda)
-    fs.writeFileSync(file, conteudoGravar);
+    console.log(azul, "**Gravar a agenda**\n", reset)
+   
+    const conteudoGravar = JSON.stringify(cadastros)
+    fs.writeFileSync("./agenda.json", conteudoGravar);
+    console.log("AGENDA GRAVADA !!!!")
+
+    readline.question("\nEnter para continuar!");
 }
 
 
+function ler_agenda(){
+    console.clear()
+    const file= "agenda.json";
+    const filebuffer=fs.readFileSync(file, "utf-8");
+
+    const contentJson= JSON.parse(filebuffer);
+
+    cadastros = contentJson;
+
+    console.log("ACÃO CONCLUIDA !!!!")
+
+    readline.question("\nEnter para continuar!");
+}
 
 
+while(true){
+    console.clear()
+   let  opcao = menu()
+    if (opcao == 1) {
+        adicionar()
+    }
+    else if (opcao == 2) {
+        listarUser()
+    }
+    else if (opcao == 3) {
+        listar()
 
-
+    }
+    else if(opcao == 4){
+        remover()
+    }
+    else if(opcao == 5){
+        limpar()
+    }
+    else if(opcao == 6){
+        gravar()
+    }
+    else if(opcao == 7){
+        ler_agenda()
+    }
+    else if(opcao == 8){
+      break;
+    }
+    else{
+        console.clear()
+        console.log(vermelho, "Opção inválida!" , reset);
+        readline.question("\nEnter para continuar!");
+  
+        
+}
+}
 
 
 
